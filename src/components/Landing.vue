ab28c6e1983efb20d985c877ddb31ce349351683
hugolpz <hugo.lpz@gmail.com>
2018-08-10 22:39:01 +0200

    add footer, clean up css

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -3,7 +3,7 @@
 
 	<section id="create" class="section">
 		<div class="has-text-centered">
-			<h2 class="title">Spice up learning with PeersGrader, for free</h2>
+			<h2 class="title">Students turn professors with PeersGrader, for free</h2>
 			<p class="subtitle">The simple way to set up peers grading activities for your class.</p>
 		</div>
 		<br>
@@ -44,10 +44,10 @@
 	</section>
 
 	<section id="dashboard" class="section has-text-centered">
-		<h3 class="title">Class dashboard</h3>
 		<div class="columns">
 			<div class="column">
-				<p class="item">Once your class has played along, followed their missions and sent up the deserved grades, the Class dashboard show the results. Students works are evaluated by both the teachers, and their peers, each with respective weight as you set it up. Students ability to fairly grade peers is also taken into account. These 3 weighted grades are combined to provide the student's final grade.</p>
+				<h3 class="title">Class dashboard</h3>
+				<p>Once your class has played along, followed their missions and sent up the deserved grades, the Class dashboard show the results. Students works are evaluated by both the teachers, and their peers, each with respective weight as you set it up. Students ability to fairly grade peers is also taken into account. These 3 weighted grades are combined to provide the student's final grade.</p>
 				<!-- We’re all about simplicity! Manage your activities from one organized place. View all your activities, ones you’ve created and participated in, from your dashboard. -->
 			</div>
 			<div class="column">
@@ -56,6 +56,18 @@
 		</div>
 	</section>
 
+	<footer class="footer" id="authors">
+		<div class="content has-text-centered">
+				<p><strong>PeersGrader</strong> by <a href="https://github.com/mystermiam">Stéphane Jabol</a> et <a href="https://github.com/hugolpz">Hugo Lopez</a>, <a href="https://cri-paris.org">CRI Paris</a> <span class="icon is-small fa-1x"><img src="../../static/images/CRI-logo-sq-textless.svg" alt="Logo CRI-paris.org"></span>.
+				<br><a href="https://opensource.org/licenses/mit-license.php">MIT License</a> & <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC-by-sa-4.0</a> for public <a href="https://en.wikipedia.org/wiki/Open_source">#OpenSource</a> & <a href="https://en.wikipedia.org/wiki/Open_data">#OpenData</a>!
+				<span class="fa-stack fa-1x">
+					<i class="fas fa-heart fa-stack-1x" style="color:#B10000;"  data-fa-transform="shrink-4 up-3"></i>
+					<i class="fas fa-hand-holding fa-stack-1x" style="color:MistyRose"></i>
+				</span>
+				<br>Cutest illustrations designed by <a href="https://flaticon.com/packs/young-avatar-collection" title="Young Avatar Collection">Freepik</a> from Flaticon (<a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>).
+				<br>Made with <span class="icon has-text-danger"><i class="fas fa-heart"></i></span> in Paris.</p>
+		</div>
+	</footer>
 	</div>
 </template>
 
@@ -72,34 +84,9 @@
 <style scoped>
 </style>
 <!--
-.pg-pg{
-	flex: 1 0 280px;
-}
-.how-it-works{
-	margin-top: 3em;
-}
-.item{
-	margin: 0.5em 0 0.5em 0;
-}
-
-.pg-column{
-	min-width: 110px;
-	max-width: 280px;
-	min-height: 240px;
-	text-align: center;
-	display: flex;
-	flex-direction: column;
-	margin: 1em 1em;
-}
-
-.img-landing-howto {
-	min-width: 100px;
-	min-height: 20px;
-	max-height: 170px;
-	max-height: 60px;
-}
-.img-landing-dashboard {
-	width: 100%;
+#howto .column,
+#dashboard .column {
+	margin: 0 1em 0 1em;
 }
 
 /*fuck this shit*/
@@ -111,12 +98,10 @@ input:focus + .icon {
 	width: 100%;
 	min-width: 100%;
 }
-
 .title-create{
 	display: flex;
 	flex-wrap: wrap;
 }
-
 #create-button{
 	width: 100%;
 }
@@ -131,9 +116,4 @@ input:focus + .icon {
 .title-create:last-child{
 	flex-grow: 1;
 }
-
-
-
-
-
  -->
