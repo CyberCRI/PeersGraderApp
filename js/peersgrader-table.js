b75387afac62b042859addca24c9ee53d4c082f4
hugolpz <hugo.lpz@gmail.com>
2019-04-23 20:31:08 +0200

    typo: number

--- js/peersgrader-table.js
+++ js/peersgrader-table.js
@@ -19,7 +19,7 @@ function tablify(data,columns,tableId) {
     .text(function (d) {
       var decimal = d.toString().split('.')[1];
       decimal&&decimal.length>0?d=d.toFixed(2):d=d;
-      return d; })
+      return +d; })
     .on('click', function (d) {
       thead.attr('class', 'header');
       if (sortAscending) {
